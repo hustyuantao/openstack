b6bf6089d89d5831acbcddfb0d7c9f4347c340d9
hustyuantao <yuan-yu-tao@163.com>
2013-10-23 15:57:10 +0800

    Prepare

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-source functions
+source prepare
 
 if [[ $EUID -eq 0 ]]; then
 	INSTALL_DEPENDENCY
 	DISABLE_SELINUX
 	INSTALL_RDO_EPEL
+	DOWNLOAD
+	START_SERVICE
+	CONFIG_RABBITMQ
+	CONFIG_MYSQL
 	
 	exit 0
 else
